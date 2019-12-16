5dbda70faf486a4f2f8701a7178e7873b1c62434
Cody <cody@acom-networks.com>
2019-12-16 16:32:38 +0800

    first commit

---
+++ Makefile
@@ -0,0 +1,2 @@
+test:
+	perl *_test.pl
---
+++ lib/Sample.pm
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+
+package Sample;
+use strict;
+use warnings;
+
+sub app {
+    sub {
+        my $env = shift;
+        [200, [], ['Hello World']];
+    };
+}
+
+1;
